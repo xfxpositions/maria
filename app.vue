0606630282b30bf8195a1465da873bf5a171c6b6
xfxpositions <xfxpositions@gmail.com>
2023-06-18 20:38:08 +0300

    initial

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
   <Navbar></Navbar>
   <NuxtPage></NuxtPage>
+  <Footer></Footer>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-12">
+    <div class="text-center flex justify-center items-center">
+      <p>Made with love by Yusuf Karaca - 2023</p>
+    </div>
+  </div>
+</template>
